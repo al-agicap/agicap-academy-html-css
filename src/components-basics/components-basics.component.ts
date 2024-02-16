d2384281ba3f4f413c1ffdd904e50d94b29e7b77
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2024-02-16 16:26:21 +0100

    feat: content projection

--- src/components-basics/components-basics.component.ts
+++ src/components-basics/components-basics.component.ts
@@ -1,35 +1,53 @@
-import { AfterViewChecked, Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HighlightService } from "../services/highlight.service";
-import { MatSnackBar, MatSnackBarConfig, MatSnackBarModule } from "@angular/material/snack-bar";
-import { TransactionFormComponent } from "./transaction-form/transaction-form.component";
-import { MatExpansionModule } from "@angular/material/expansion";
+import {CommonModule} from "@angular/common";
+import {AfterViewChecked, Component} from "@angular/core";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatSnackBar, MatSnackBarConfig, MatSnackBarModule} from "@angular/material/snack-bar";
+import {HighlightService} from "../services/highlight.service";
+import {MyCardComponent} from "./my-card/my-card.component";
+import {TransactionFormComponent} from "./transaction-form/transaction-form.component";
 
 @Component({
-  selector: "app-components-basics",
-  standalone: true,
-  imports: [CommonModule, TransactionFormComponent, MatSnackBarModule, MatExpansionModule],
-  templateUrl: "./components-basics.component.html",
-  styleUrls: ["./components-basics.component.scss"],
-  providers: [HighlightService, MatSnackBarConfig]
+    selector: "app-components-basics",
+    standalone: true,
+    imports: [CommonModule, TransactionFormComponent, MatSnackBarModule, MatExpansionModule, MyCardComponent],
+    templateUrl: "./components-basics.component.html",
+    styleUrls: ["./components-basics.component.scss"],
+    providers: [HighlightService, MatSnackBarConfig]
 })
 export class ComponentsBasicsComponent implements AfterViewChecked {
 
-  public billingDate = new Date();
+    public billingDate = new Date();
 
-  public transactionAmount = 1000;
+    public transactionAmount = 1000;
 
-  public componentTemplate = "<app-transaction-form (validate)=\"validate($event)\" [billingDate]=\"billingDate\" transactionAmount=\"1000\"></app-transaction-form>";
+    public componentTemplate = "<app-transaction-form (validate)=\"validate($event)\" [billingDate]=\"billingDate\" transactionAmount=\"1000\"></app-transaction-form>";
+    contentProjectionTemplate: string = `
+    <-- my card component -->
+    <header>
+        <ng-content select="[title]"></ng-content>
+    </header>
+    <div class="content">
+        <ng-content></ng-content>
+    </div>
+    <footer></footer>
 
-  constructor(private snack: MatSnackBar, private highlightService: HighlightService) {
-  }
 
-  ngAfterViewChecked() {
-    this.highlightService.highlightAll();
-  }
+    <-- parent component -->
+    <app-my-card>
+        <div title>Title</div>
+        <div class="content">Content</div>
+    </app-my-card>
+  `;
 
-  public validate(): void {
-    this.snack.open("Validation completed", "Close", {});
-  }
+    constructor(private snack: MatSnackBar, private highlightService: HighlightService) {
+    }
+
+    ngAfterViewChecked() {
+        this.highlightService.highlightAll();
+    }
+
+    public validate(): void {
+        this.snack.open("Validation completed", "Close", {});
+    }
 
 }
---
+++ src/components-basics/my-card/my-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-my-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './my-card.component.html',
+  styleUrls: ['./my-card.component.scss']
+})
+export class MyCardComponent {
+
+}
