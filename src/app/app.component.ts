944adf47e62299119a40366924c3a919496d3e23
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2024-02-16 10:17:27 +0100

    feat: update tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from "@angular/core";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatTabsModule } from "@angular/material/tabs";
-import { ExampleDirectivesComponent } from "../example-directives/example-directives.component";
-import { FlipperComponent } from "../flipper/flipper.component";
-import { HtmlCssBestPraticesComponent } from "../html-css-best-pratices/html-css-best-pratices.component";
-import { CssEncapsulationComponent } from "../css-encapsulation/css-encapsulation.component";
+import {Component, OnInit} from "@angular/core";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatTabsModule} from "@angular/material/tabs";
+import {CssEncapsulationComponent} from "../css-encapsulation/css-encapsulation.component";
+import {ExampleDirectivesComponent} from "../example-directives/example-directives.component";
+import {FlexGridComponent} from "../flex-grid/flex-grid.component";
+import {FlipperComponent} from "../flipper/flipper.component";
+import {HtmlCssBestPraticesComponent} from "../html-css-best-pratices/html-css-best-pratices.component";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  standalone: true,
-  styleUrls: ["./app.component.scss"],
-  imports: [MatExpansionModule, MatTabsModule, ExampleDirectivesComponent, FlipperComponent, HtmlCssBestPraticesComponent, CssEncapsulationComponent]
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    standalone: true,
+    styleUrls: ["./app.component.scss"],
+    imports: [MatExpansionModule, MatTabsModule, ExampleDirectivesComponent, FlipperComponent, HtmlCssBestPraticesComponent, CssEncapsulationComponent, FlexGridComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    selectedIndex = 0;
 
+    ngOnInit(): void {
+        this.selectedIndex = Number(localStorage.getItem('selectedIndex') ?? 0);
+    }
+
+    onTabChange($event: number) {
+        localStorage.setItem('selectedIndex', $event.toString());
+    }
 }
---
+++ src/flex-grid/flex-grid.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-flex-grid',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './flex-grid.component.html',
+  styleUrls: ['./flex-grid.component.scss']
+})
+export class FlexGridComponent {
+
+}
