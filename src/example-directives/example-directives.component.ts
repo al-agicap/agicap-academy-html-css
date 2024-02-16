8cb7a5e89ec48344d456c1212db08a847dc93f6e
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2024-02-16 10:43:21 +0100

    feat: add support for code highlight

--- src/example-directives/example-directives.component.ts
+++ src/example-directives/example-directives.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {ExampleWithFlipperComponent} from "../flipper/example-with-flipper/example-with-flipper.component";
 
 @Component({
-  selector: 'app-example-directives',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './example-directives.component.html',
-  styleUrls: ['./example-directives.component.scss']
+    selector: 'app-example-directives',
+    standalone: true,
+    imports: [CommonModule, ExampleWithFlipperComponent],
+    templateUrl: './example-directives.component.html',
+    styleUrls: ['./example-directives.component.scss']
 })
 export class ExampleDirectivesComponent {
+
+    withFlipper = false;
+    code = `
+   <mat-tab-group [selectedIndex]="selectedIndex" (selectedIndexChange)="onTabChange($event)">
+    <mat-tab label="HTML best practices">
+        <app-html-css-best-pratices></app-html-css-best-pratices>
+    </mat-tab>
+    <mat-tab label="Flipper">
+        <app-flipper></app-flipper>
+    </mat-tab>
+    <mat-tab label="CSS encapsulation">
+        <app-css-encapsulation></app-css-encapsulation>
+    </mat-tab>
+    <mat-tab label="Directives Angular">
+        <app-example-directives></app-example-directives>
+    </mat-tab>
+    <mat-tab label="Flex & CSS Grid">
+        <app-flex-grid></app-flex-grid>
+    </mat-tab>
+
+</mat-tab-group>
+
+
+    `
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { FLIPPER_ICONS_INITIALIZER } from "@agicap/flipper/icons";
-import { HttpClientModule } from "@angular/common/http";
-import { importProvidersFrom } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
+import {provideFlipperDateAdapter} from "@agicap/flipper/datetime";
+import {FLIPPER_ICONS_INITIALIZER} from "@agicap/flipper/icons";
+import {HttpClientModule} from "@angular/common/http";
+import {importProvidersFrom} from "@angular/core";
+import {bootstrapApplication} from "@angular/platform-browser";
 import "zone.js";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideFlipperDateAdapter } from "@agicap/flipper/datetime";
-import { AppComponent } from "./app/app.component";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {AppComponent} from "./app/app.component";
+import 'prismjs';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-scss';
 
 bootstrapApplication(AppComponent, {
     providers: [provideAnimations(), importProvidersFrom(HttpClientModule), FLIPPER_ICONS_INITIALIZER, provideFlipperDateAdapter()]
