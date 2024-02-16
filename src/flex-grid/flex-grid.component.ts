76d94a8da0d4a9a172d5d6e062e50511fcf78c59
Paul Sorrentino <paul.sorrentino@agicap.com>
2024-02-16 15:20:35 +0100

    feat: expansion flex grid

--- src/flex-grid/flex-grid.component.ts
+++ src/flex-grid/flex-grid.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FlexComponent } from "./flex/flex.component";
 import { GridComponent } from "./grid/grid.component";
+import { MatExpansionModule } from "@angular/material/expansion";
 
 @Component({
   selector: "app-flex-grid",
   standalone: true,
-  imports: [CommonModule, FlexComponent, GridComponent],
+  imports: [CommonModule, FlexComponent, GridComponent, MatExpansionModule],
   templateUrl: "./flex-grid.component.html",
   styleUrls: ["./flex-grid.component.scss"]
 })
